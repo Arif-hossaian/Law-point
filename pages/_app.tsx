96395d08d7745e998934819f7b5bd49ecd17a4ea
Arif-hossaian <arifhossain42136@gmail.com>
2022-09-21 13:04:58 +0600

    fix some bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <ThemeProvider attribute="class">

773eff9090536a061e5b65ca98e0384cb8df2201
Arif-hossaian <arifhossain42136@gmail.com>
2022-09-21 13:10:32 +0600

    change button color

--- components/LawyerInfo/LawyerInfo.tsx
+++ components/LawyerInfo/LawyerInfo.tsx
@@ -52,7 +52,11 @@ const LawyerInfo: FC<LawyerInfoProps> = ({
             <Button>
               <a href={phone}>Call Now</a>
             </Button>
-            <Button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            <Button
+              variant="outline"
+              size="md"
+              className="bg-transparent   font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            >
               <a href={email}>Send Email</a>
             </Button>
           </div>

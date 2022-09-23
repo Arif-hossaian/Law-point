a7261999dc7619c0d15a237cf8053127252d0c15
Arif-hossaian <arifhossain42136@gmail.com>
2022-09-23 23:50:29 +0600

    change context

--- components/Banner-section/Banner.tsx
+++ components/Banner-section/Banner.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import useTypingText from '../../hooks/useTypingText';
+import React, { useRef, useEffect } from 'react';
 import BannerImage from '../../icons/58918-legal-statement.json';
 import ImageAnimation from '../../shared/ImageAnimation';
+import Typed from 'typed.js';
 
 const Banner: React.FC = () => {
-  const { typingText } = useTypingText(
-    'If you tremble with indignation at every injustice then you are a comrade of mine',
-    80,
-    100
-  );
+  const el = useRef<HTMLSpanElement | any>(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Lawyers', 'Laws', 'Helpline'], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 100,
+      smartBackspace: true,
+      loop: true,
+      showCursor: true,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div className="py-16 px-20 bg-gray-200 dark:bg-gray-700">
       <div className="mx-auto mt-16 w-full">
@@ -18,11 +33,17 @@ const Banner: React.FC = () => {
            sm:px-8 md:flex-row"
         >
           <h1>
-            <span className="mb-4 inline-block text-4xl font-bold text-gray-500 dark:text-gray-50 md:text-6xl">
-              {'Search your'} <span className="text-indigo-600">{'Laws'}</span>
+            <span className="mb-4 inline-block text-2xl font-bold text-gray-500 dark:text-gray-50 md:text-6xl">
+              {'Find out your desire'}
             </span>
-            <span className="mt-2 block w-72 text-gray-600 dark:text-gray-50">
-              {typingText}
+            <br></br>
+            <span
+              className="text-indigo-600 md:text-4xl mt-1 text-lg mb-2"
+              ref={el}
+            ></span>
+            <span className="mt-4 block w-72 text-gray-600 dark:text-gray-50">
+              If you tremble with indignation at every injustice then you are a
+              comrade of mine
             </span>
           </h1>
 
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -30,22 +30,7 @@ const ContactForm: FC = () => {
                   htmlFor="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:main-text peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
-                  First name
-                </label>
-              </div>
-              <div className="relative z-0 mb-10 w-full group">
-                <input
-                  type="text"
-                  name="floating_last_name"
-                  id="floating_last_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:main-text peer"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="floating_last_name"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:main-text peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Last name
+                  Full name
                 </label>
               </div>
 
@@ -81,21 +66,7 @@ const ContactForm: FC = () => {
                   Phone number (01X-XXX-XX-XX)
                 </label>
               </div>
-              <div className="relative z-0 mb-10 w-full group">
-                <input
-                  type="text"
-                  name="floating_company"
-                  id="floating_company"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:main-text peer"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="floating_company"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:main-text peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Company Name
-                </label>
-              </div>
+
               <div className="relative z-0 mb-10 w-full group">
                 <textarea
                   id="message"
